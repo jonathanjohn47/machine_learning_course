94cab90bbd520b0259447b8e133af4d612ea7d1f
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2023-07-01 16:34:56 +0530

    Heirarchical Clustering

---
+++ heirarchichal_clustering.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+from sklearn.preprocessing import normalize
+from sklearn.cluster import AgglomerativeClustering
+
+import scipy.cluster.hierarchy as shc
+
+data = DataFrame(np.random.randint(0, 20000, size=(1000, 2)))
+
+#data_scaled = DataFrame(normalize(data))
+
+plt.figure(figsize=(10, 7))
+plt.title("Customer Dendograms")
+dend = shc.dendrogram(shc.linkage(data, method='ward'))
+plt.show()
+
+cluster = AgglomerativeClustering(n_clusters=3, affinity='euclidean', linkage='ward')
+cluster.fit_predict(data)
+
+
+plt.scatter(data[0], data[1], c=cluster.labels_, cmap='rainbow')
+plt.show()
